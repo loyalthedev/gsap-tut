632b56f647920588683b631fdc82827aeedf2eb2
loyalthedev <lifeofemmany6@gmail.com>
2025-07-20 19:28:46 +0800

    Start and end gsap properties

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 gsap.registerPlugin("ScrollTrigger");
 
 gsap.to(".square", {
-  x: 700,
+  // x: 700,
   duration: 3,
-  scrollTrigger: ".square2",
+  scrollTrigger: {
+    trigger: ".square",
+    start: "top 30%",
+    end: () => `+=${document.querySelector(".square").offsetHeight}`,
+    markers: true,
+    toggleClass: "red",
+  },
 });
+
+//  markers: {
+//       startColor: "purple",
+//       endColor: "fuchsia",
+//       fontSize: "4rem",
+//       indent: 200,
+//     },
